d7547acff344361fd9beac9a1550833941ebe961
can-dy-jack <jjack_chen3@163.com>
2021-05-09 10:50:43 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ColorChoose
 A tool of choosing  Chinese traditional color.
+
+这是一个模仿网站，原网站：[https://color.uisdc.com](https://color.uisdc.com/)
+
---
+++ index.js
@@ -0,0 +1,6 @@
+const column = document.getElementsByClassName('column');
+
+column.forEach(addEventListener('click',()=>{
+    window.alert('已复制！');
+}))
+
