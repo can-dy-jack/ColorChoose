9a824e3c14672c30e847ff424a559bb96414e30a
can-dy-jack <jjack_chen3@163.com>
2021-05-29 17:37:35 +0800

    fix CopyDownText

--- clipboard.js
+++ clipboard.js
@@ -4,14 +4,24 @@
 const colorbox = document.getElementsByClassName('colorbox');
 const colorText = document.getElementsByTagName('textarea');
 
-// 提示框
+// CopyDownText  提示框
 let hint = document.createElement('div');
 let hinttext=document.createTextNode("已复制到剪贴板");
 hint.className = "hintText";
 hint.appendChild(hinttext);
 hint.hidden = true;
+
+// 鼠标指向提示框
+// let MouseOver = document.createElement('div');
+// let MouseOverText=document.createTextNode("点击复制到剪贴板");
+// MouseOver.className = "MouseOverText";
+// MouseOver.appendChild(MouseOverText);
+
 // 复制到剪贴板
 for (let i = 0;i<colorbox.length;i++){
+    // colorbox[i].addEventListener('mouseover',()=>{
+    //     colorbox[i].appendChild(MouseOver);
+    // })
     colorbox[i].addEventListener('click',()=>{
         colorText[i].select();
         colorbox[i].appendChild(hint);
@@ -21,5 +31,10 @@ for (let i = 0;i<colorbox.length;i++){
             hint.hidden = false;
         }
     })
+    // colorbox[i].addEventListener('mouseout',()=>{
+        // if (colorbox[i].contains(MouseOver)){
+        //     colorbox[i].removeChild(MouseOver);
+        // }
+    // })
 }
 
